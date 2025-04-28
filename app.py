f6653d1bfadfc78c5d0afa220c466cf9e440d42d
Anderson Lucas <114498251+andersoncsgo@users.noreply.github.com>
2025-04-28 16:15:43 -0300

    Create payment_confirmed route

--- app.py
+++ app.py
@@ -40,22 +40,46 @@ def create_paytment_pix():
 @app.route('/payments/pix/qr_code/<file_name>', methods=['GET'])
 def get_image(file_name):
     return send_file(f"static/img/{file_name}.png", mimetype='image/png')
-
+# payment
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
+    data = request.get_json()
+    #value
+
+    # validations
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message": "invalid payment data"}), 400
+
+    payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
+
+    if not payment or payment.paid:
+        return jsonify({"message": "Payment not found"}), 404
+
+    if data.get("value") != payment.value:
+        return jsonify({"message": "invalid payment data"}), 400
+
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
     return jsonify({"message": "The payment has been confirmed"})
+
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
-
+    if payment.paid:
+         return render_template('confirmed_payment.html',
+                                payment_id=payment.id,
+                                value=payment.value
+                                )
     return render_template('payment.html',
                            payment_id=payment.id,
                            value=payment.value,
                            host="http://127.0.0.1:5000",
                            qr_code=payment.qr_code,
                            )
+
 
 
 # websockets
